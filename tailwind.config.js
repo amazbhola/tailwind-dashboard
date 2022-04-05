c783afdd93d3cd1309e04c683bd231c175b560ad
amazbhola <amazingbhola@gmail.com>
2022-04-05 16:02:41 +0600

    This Configure is Tailwind and Webpack

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // purge: ['./dist/*.html',],
+  darkMode:false,
+  content: ["./src/**/*.{html,js}",'*.{html,js}'],
+  // content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  variants:{
+    extend:{}
+  }
+}
