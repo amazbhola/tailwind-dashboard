e3acf83f0cecd0032866447964881d5a541df53a
amazbhola <amazingbhola@gmail.com>
2022-04-15 19:26:15 +0600

    create html design with tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/*.html'],
   mode:'jit',
-  darkMode:false,
+  darkMode:'class',
   content: ["./src/**/*.{html,js}",'*.{html,js}'],
   // content: [],
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins'],
+        poppins:""
      }
     },
   },
